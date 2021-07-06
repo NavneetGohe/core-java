daab31bf7ad83298e4efda826b4cbb5a0f5147f6
NavneetGohe <gohenavneet6@gmail.com>
2021-07-06 04:29:35 -0700

    my project

---
+++ AddName.java
@@ -0,0 +1,14 @@
+package com;
+
+public class AddName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String name = "Navneet";
+int i = 0;
+while (i<5) {
+System.out.println(i+".Hello " + name);
+i++;
+	}}
+
+}
---
+++ Percentage.java
@@ -0,0 +1,16 @@
+package com;
+
+public class Percentage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a = 75; 
+int b = 80;
+int c = 67;
+int d = 89;
+int e = 76;
+int z = a+b+c+d+e;
+System.out.println(z/5);
+	}
+
+}
